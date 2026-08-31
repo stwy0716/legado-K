75e5b9eb7b17a81348328eefe6f10f978ed093b9
Legado MD3 <legado@md3.app>
2026-08-31 17:16:03 +0800

    核心修复: 数据库表结构+模型字段全面对齐原版
    数据库:
    - books表新增bookUrl列
    - type字段从TEXT改为INTEGER
    - order字段统一为order_num
    - book_sources表新增exploreScreen/ruleReview/coverDecodeJs/eventListener/customButton/homepageModules
    - 版本升级到3，重建所有表
    - 新增getBook/updateReadPosition方法
    - 删除书籍时级联删除书签和阅读记录
    
    模型:
    - Book: type改为int, 新增toJson/fromJson/toJsonString
    - BookChapter: 列名对齐数据库(chapter_index/start_pos/end_pos)
    - BookSource: 新增原版字段, 新增toMap/fromMap兼容数据库
    - ReplaceRule: order字段对齐order_num

--- lib/models/book_chapter.dart
+++ lib/models/book_chapter.dart
@@ -2,10 +2,10 @@ class BookChapter {
   String title;
   String url;
   int index;
-  bool? isVolume;
+  bool isVolume;
   String? content;
-  int? start;
-  int? end;
+  int? startPos;
+  int? endPos;
   String? variable;
 
   BookChapter({
@@ -14,30 +14,32 @@ class BookChapter {
     required this.index,
     this.isVolume = false,
     this.content,
-    this.start,
-    this.end,
+    this.startPos,
+    this.endPos,
     this.variable,
   });
 
-  Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap(String bookName, String bookAuthor) => {
+    'bookName': bookName,
+    'bookAuthor': bookAuthor,
     'title': title,
     'url': url,
-    'index': index,
-    'isVolume': isVolume == true ? 1 : 0,
+    'chapter_index': index,
+    'isVolume': isVolume ? 1 : 0,
     'content': content,
-    'start': start,
-    'end': end,
+    'start_pos': startPos,
+    'end_pos': endPos,
     'variable': variable,
   };
 
   factory BookChapter.fromMap(Map<String, dynamic> map) => BookChapter(
     title: map['title'] as String,
     url: map['url'] as String,
-    index: map['index'] as int,
-    isVolume: (map['isVolume'] as int?) == 1,
+    index: map['chapter_index'] as int? ?? map['index'] as int? ?? 0,
+    isVolume: (map['isVolume'] as int? ?? 0) == 1,
     content: map['content'] as String?,
-    start: map['start'] as int?,
-    end: map['end'] as int?,
+    startPos: map['start_pos'] as int? ?? map['start'] as int?,
+    endPos: map['end_pos'] as int? ?? map['end'] as int?,
     variable: map['variable'] as String?,
   );
 }
