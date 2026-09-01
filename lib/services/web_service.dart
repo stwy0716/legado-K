3b651813eb12a4bf727db65162137d322f6e63a2
stwy0716 <191060000+stwy0716@users.noreply.github.com>
2026-09-01 08:25:14 +0800

    修复BookSource字段名: 使用toJson()而非手动字段

--- lib/services/web_service.dart
+++ lib/services/web_service.dart
@@ -97,7 +97,7 @@ class WebService {
   // === 书源API ===
   Future<Response> _getBookSources(Request request) async {
     final sources = await _db.getAllSources();
-    return _jsonResponse(sources.map((s) => {'name': s.name, 'url': s.url, 'group': s.group, 'enabled': s.enabled}).toList());
+    return _jsonResponse(sources.map((s) => s.toJson()).toList());
   }
 
   Future<Response> _getBookSource(Request request) async {
@@ -235,7 +235,7 @@ class WebService {
   // === RSS API ===
   Future<Response> _getRssSources(Request request) async {
     final sources = await _db.getRssSources();
-    return _jsonResponse(sources.map((s) => {'name': s.name, 'url': s.url, 'group': s.group, 'enabled': s.enabled}).toList());
+    return _jsonResponse(sources.map((s) => s.toJson()).toList());
   }
 
   Future<Response> _saveRssSource(Request request) async {
