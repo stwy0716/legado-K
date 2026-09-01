0a06b505b97ce0d5ff0139efbf382f545a922ad5
Legado MD3 <legado@md3.app>
2026-09-01 12:24:54 +0800

    新增: 全面阅读设置屏幕(5Tab)
    - 文字Tab: 字号/粗体/字重/对齐/颜色/字体
    - 排版Tab: 缩进/行距/段距/字间距/阴影/下划线/边距
    - 翻页Tab: 动画/点击翻页/音量键/自动翻页/模拟阅读/颠倒
    - 显示Tab: 状态栏/标题/时间/电量/页码/进度/沉浸/全屏/方向/护眼
    - 其他Tab: 常亮/菜单/长按/编码/预下载/自定义头尾
    - 重写ReadConfig模型(60+字段)
    - 阅读屏幕设置按钮导航到新屏幕
    - GitHub Actions: 创建新预发布时自动删除旧预发布

--- lib/data/model/read_config.dart
+++ lib/data/model/read_config.dart
@@ -1,32 +1,53 @@
+import 'dart:convert';
+
+/// 阅读配置
 class ReadConfig {
+  // 文字
   int textSize;
-  int bgColor;
   int textColor;
-  String? bgImage;
-  int pageAnim; // 0:覆盖 1:仿真 2:滑动 3:滚动 4:无动画 5:上下
+  String? fontFamily;
+  int fontWeight;
+  bool boldText;
+  int textAlign; // 0:左对齐 1:居中 2:两端对齐
   int textIndent;
   int lineSpacing;
   int paragraphSpacing;
+  double letterSpacing;
+  double wordSpacing;
+
+  // 背景
+  int bgColor;
+  String? bgImage;
+  int shadowLevel; // 0-3
+  int underlineType; // 0:无 1:实线 2:虚线 3:波浪线 4:双线
+
+  // 翻页
+  int pageAnim; // 0:覆盖 1:仿真 2:滑动 3:滚动 4:无动画 5:上下
+  bool invertPage;
+  int clickAreaNext;
+  int clickAreaPrev;
+  bool clickTurnPage;
+  bool volumeKeyPage;
+  bool volumeKeyReverse;
+  bool autoNextPage;
+  int autoNextPageSpeed;
+  bool simulatedReading;
+
+  // 显示
   bool statusBarVisibility;
   bool titleVisibility;
   bool timeVisibility;
   bool batteryVisibility;
   bool pageNumberVisibility;
-  String? fontFamily;
-  int fontWeight;
-  bool boldText;
-  int textAlign; // 0:左对齐 1:居中 2:两端对齐
-  int verticalLayout; // 0:横屏 1:竖屏
-  bool invertPage;
-  int clickAreaNext;
-  int clickAreaPrev;
-  bool volumeKeyPage;
-  bool volumeKeyReverse;
-  bool autoNextPage;
-  int autoNextPageSpeed;
-  bool keepScreenOn;
-  bool showMenuOnTap;
+  bool showProgress;
+  bool immersiveMode;
+  bool fullScreen;
+  int screenOrientation; // 0:自动 1:竖屏 2:横屏
   bool eyeProtection;
+  int eyeProtectionLevel;
+
+  // 布局
+  int verticalLayout; // 0:横屏 1:竖屏
   int paddingLeft;
   int paddingRight;
   int paddingTop;
@@ -39,104 +60,162 @@ class ReadConfig {
   int footerSize;
   bool headerBold;
   bool footerBold;
-  int headerColor;
-  int footerColor;
-  bool headerAlignCenter;
-  bool footerAlignCenter;
+
+  // 其他
+  bool keepScreenOn;
+  bool showMenuOnTap;
+  bool longPressSelect;
+  String? charset;
+  int preDownloadCount;
+  bool showTimeBattery;
+  bool customHeaderEnabled;
+  bool customFooterEnabled;
 
   ReadConfig({
     this.textSize = 20,
-    this.bgColor = 0xFFFFF8E1,
     this.textColor = 0xFF333333,
-    this.bgImage,
-    this.pageAnim = 0,
+    this.fontFamily,
+    this.fontWeight = 400,
+    this.boldText = false,
+    this.textAlign = 2,
     this.textIndent = 2,
     this.lineSpacing = 2,
     this.paragraphSpacing = 1,
+    this.letterSpacing = 0,
+    this.wordSpacing = 0,
+    this.bgColor = 0xFFFFF8E1,
+    this.bgImage,
+    this.shadowLevel = 0,
+    this.underlineType = 0,
+    this.pageAnim = 0,
+    this.invertPage = false,
+    this.clickAreaNext = 1,
+    this.clickAreaPrev = 1,
+    this.clickTurnPage = true,
+    this.volumeKeyPage = false,
+    this.volumeKeyReverse = false,
+    this.autoNextPage = false,
+    this.autoNextPageSpeed = 5,
+    this.simulatedReading = false,
     this.statusBarVisibility = true,
     this.titleVisibility = true,
     this.timeVisibility = true,
     this.batteryVisibility = true,
     this.pageNumberVisibility = true,
-    this.fontFamily,
-    this.fontWeight = 400,
-    this.boldText = false,
-    this.textAlign = 2,
-    this.verticalLayout = 0,
-    this.invertPage = false,
-    this.clickAreaNext = 1,
-    this.clickAreaPrev = 1,
-    this.volumeKeyPage = true,
-    this.volumeKeyReverse = false,
-    this.autoNextPage = false,
-    this.autoNextPageSpeed = 10,
-    this.keepScreenOn = true,
-    this.showMenuOnTap = true,
+    this.showProgress = true,
+    this.immersiveMode = false,
+    this.fullScreen = false,
+    this.screenOrientation = 0,
     this.eyeProtection = false,
+    this.eyeProtectionLevel = 50,
+    this.verticalLayout = 0,
     this.paddingLeft = 16,
     this.paddingRight = 16,
     this.paddingTop = 8,
     this.paddingBottom = 8,
-    this.headerHeight = 24,
-    this.footerHeight = 24,
+    this.headerHeight = 32,
+    this.footerHeight = 32,
     this.headerString,
     this.footerString,
     this.headerSize = 12,
     this.footerSize = 12,
     this.headerBold = false,
     this.footerBold = false,
-    this.headerColor = 0xFF888888,
-    this.footerColor = 0xFF888888,
-    this.headerAlignCenter = false,
-    this.footerAlignCenter = true,
+    this.keepScreenOn = false,
+    this.showMenuOnTap = true,
+    this.longPressSelect = true,
+    this.charset,
+    this.preDownloadCount = 0,
+    this.showTimeBattery = true,
+    this.customHeaderEnabled = false,
+    this.customFooterEnabled = false,
   });
 
-  static const List<Map<String, dynamic>> presets = [
-    {'name': '护眼', 'bg': 0xFFCCE8CF, 'text': 0xFF333333},
-    {'name': '羊皮纸', 'bg': 0xFFF1E9D2, 'text': 0xFF5B4636},
-    {'name': '纯白', 'bg': 0xFFFFFFFF, 'text': 0xFF333333},
-    {'name': '白绿', 'bg': 0xFFE0F0E0, 'text': 0xFF333333},
-    {'name': '黑色', 'bg': 0xFF000000, 'text': 0xFFBBBBBB},
-    {'name': '深灰', 'bg': 0xFF2B2B2B, 'text': 0xFFBBBBBB},
-    {'name': '褐色', 'bg': 0xFF3B2F2F, 'text': 0xFFC9B8A8},
-    {'name': '绿色', 'bg': 0xFF0B3D0B, 'text': 0xFF8FBC8F},
-  ];
-
   Map<String, dynamic> toJson() => {
-    'textSize': textSize,
-    'bgColor': bgColor,
-    'textColor': textColor,
-    'bgImage': bgImage,
-    'pageAnim': pageAnim,
-    'textIndent': textIndent,
-    'lineSpacing': lineSpacing,
-    'paragraphSpacing': paragraphSpacing,
-    'fontFamily': fontFamily,
-    'boldText': boldText,
-    'textAlign': textAlign,
-    'verticalLayout': verticalLayout,
-    'paddingLeft': paddingLeft,
-    'paddingRight': paddingRight,
-    'paddingTop': paddingTop,
-    'paddingBottom': paddingBottom,
+    'textSize': textSize, 'textColor': textColor, 'fontFamily': fontFamily,
+    'fontWeight': fontWeight, 'boldText': boldText, 'textAlign': textAlign,
+    'textIndent': textIndent, 'lineSpacing': lineSpacing, 'paragraphSpacing': paragraphSpacing,
+    'letterSpacing': letterSpacing, 'wordSpacing': wordSpacing,
+    'bgColor': bgColor, 'bgImage': bgImage, 'shadowLevel': shadowLevel, 'underlineType': underlineType,
+    'pageAnim': pageAnim, 'invertPage': invertPage, 'clickAreaNext': clickAreaNext, 'clickAreaPrev': clickAreaPrev,
+    'clickTurnPage': clickTurnPage, 'volumeKeyPage': volumeKeyPage, 'volumeKeyReverse': volumeKeyReverse,
+    'autoNextPage': autoNextPage, 'autoNextPageSpeed': autoNextPageSpeed, 'simulatedReading': simulatedReading,
+    'statusBarVisibility': statusBarVisibility, 'titleVisibility': titleVisibility, 'timeVisibility': timeVisibility,
+    'batteryVisibility': batteryVisibility, 'pageNumberVisibility': pageNumberVisibility, 'showProgress': showProgress,
+    'immersiveMode': immersiveMode, 'fullScreen': fullScreen, 'screenOrientation': screenOrientation,
+    'eyeProtection': eyeProtection, 'eyeProtectionLevel': eyeProtectionLevel,
+    'verticalLayout': verticalLayout, 'paddingLeft': paddingLeft, 'paddingRight': paddingRight,
+    'paddingTop': paddingTop, 'paddingBottom': paddingBottom,
+    'headerHeight': headerHeight, 'footerHeight': footerHeight,
+    'headerString': headerString, 'footerString': footerString,
+    'headerSize': headerSize, 'footerSize': footerSize,
+    'headerBold': headerBold, 'footerBold': footerBold,
+    'keepScreenOn': keepScreenOn, 'showMenuOnTap': showMenuOnTap, 'longPressSelect': longPressSelect,
+    'charset': charset, 'preDownloadCount': preDownloadCount,
+    'showTimeBattery': showTimeBattery,
+    'customHeaderEnabled': customHeaderEnabled, 'customFooterEnabled': customFooterEnabled,
   };
 
   factory ReadConfig.fromJson(Map<String, dynamic> json) => ReadConfig(
     textSize: json['textSize'] ?? 20,
-    bgColor: json['bgColor'] ?? 0xFFFFF8E1,
     textColor: json['textColor'] ?? 0xFF333333,
-    bgImage: json['bgImage'],
-    pageAnim: json['pageAnim'] ?? 0,
+    fontFamily: json['fontFamily'],
+    fontWeight: json['fontWeight'] ?? 400,
+    boldText: json['boldText'] ?? false,
+    textAlign: json['textAlign'] ?? 2,
     textIndent: json['textIndent'] ?? 2,
     lineSpacing: json['lineSpacing'] ?? 2,
     paragraphSpacing: json['paragraphSpacing'] ?? 1,
-    fontFamily: json['fontFamily'],
-    boldText: json['boldText'] ?? false,
-    textAlign: json['textAlign'] ?? 2,
+    letterSpacing: (json['letterSpacing'] ?? 0).toDouble(),
+    wordSpacing: (json['wordSpacing'] ?? 0).toDouble(),
+    bgColor: json['bgColor'] ?? 0xFFFFF8E1,
+    bgImage: json['bgImage'],
+    shadowLevel: json['shadowLevel'] ?? 0,
+    underlineType: json['underlineType'] ?? 0,
+    pageAnim: json['pageAnim'] ?? 0,
+    invertPage: json['invertPage'] ?? false,
+    clickAreaNext: json['clickAreaNext'] ?? 1,
+    clickAreaPrev: json['clickAreaPrev'] ?? 1,
+    clickTurnPage: json['clickTurnPage'] ?? true,
+    volumeKeyPage: json['volumeKeyPage'] ?? false,
+    volumeKeyReverse: json['volumeKeyReverse'] ?? false,
+    autoNextPage: json['autoNextPage'] ?? false,
+    autoNextPageSpeed: json['autoNextPageSpeed'] ?? 5,
+    simulatedReading: json['simulatedReading'] ?? false,
+    statusBarVisibility: json['statusBarVisibility'] ?? true,
+    titleVisibility: json['titleVisibility'] ?? true,
+    timeVisibility: json['timeVisibility'] ?? true,
+    batteryVisibility: json['batteryVisibility'] ?? true,
+    pageNumberVisibility: json['pageNumberVisibility'] ?? true,
+    showProgress: json['showProgress'] ?? true,
+    immersiveMode: json['immersiveMode'] ?? false,
+    fullScreen: json['fullScreen'] ?? false,
+    screenOrientation: json['screenOrientation'] ?? 0,
+    eyeProtection: json['eyeProtection'] ?? false,
+    eyeProtectionLevel: json['eyeProtectionLevel'] ?? 50,
     verticalLayout: json['verticalLayout'] ?? 0,
     paddingLeft: json['paddingLeft'] ?? 16,
     paddingRight: json['paddingRight'] ?? 16,
     paddingTop: json['paddingTop'] ?? 8,
     paddingBottom: json['paddingBottom'] ?? 8,
+    headerHeight: json['headerHeight'] ?? 32,
+    footerHeight: json['footerHeight'] ?? 32,
+    headerString: json['headerString'],
+    footerString: json['footerString'],
+    headerSize: json['headerSize'] ?? 12,
+    footerSize: json['footerSize'] ?? 12,
+    headerBold: json['headerBold'] ?? false,
+    footerBold: json['footerBold'] ?? false,
+    keepScreenOn: json['keepScreenOn'] ?? false,
+    showMenuOnTap: json['showMenuOnTap'] ?? true,
+    longPressSelect: json['longPressSelect'] ?? true,
+    charset: json['charset'],
+    preDownloadCount: json['preDownloadCount'] ?? 0,
+    showTimeBattery: json['showTimeBattery'] ?? true,
+    customHeaderEnabled: json['customHeaderEnabled'] ?? false,
+    customFooterEnabled: json['customFooterEnabled'] ?? false,
   );
+
+  String toJsonString() => jsonEncode(toJson());
+  factory ReadConfig.fromJsonString(String str) => ReadConfig.fromJson(jsonDecode(str));
 }
