cce05506256ec035771851454c1d9da310916bd9
stwy0716 <191060000+stwy0716@users.noreply.github.com>
2026-09-06 08:50:37 +0800

    修复阅读背景图Image.file缺少dart:io导入的编译错误

--- lib/ui/book/read/reading_screen.dart
+++ lib/ui/book/read/reading_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:legado_md3/data/model/bookmark.dart';
